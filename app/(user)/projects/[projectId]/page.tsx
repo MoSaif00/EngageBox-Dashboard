48ee632cb23bd9a814845e22c0a414d8614a125d
Mo.Saif <moh14ab@gmail.com>
2025-01-01 02:41:05 +0100

    fix

--- app/(user)/projects/[projectId]/page.tsx
+++ app/(user)/projects/[projectId]/page.tsx
@@ -5,30 +5,33 @@ import Link from "next/link";
 import { ChevronLeft, Code, Globe } from "lucide-react";
 import Table from "@/components/table";
 
-
-const page = async ({ params }: {
+type PageProps = {
     params: {
         projectId: string;
     };
-}) => {
-    const { projectId } = await params;
+};
+
+const page = async ({ params }: PageProps) => {
+    const { projectId } = params; // No need for `await` here
 
     if (!projectId) {
-        return (<div>Invalid Project ID</div>);
+        return <div>Invalid Project ID</div>;
     }
 
     const userProjects = await db.query.projects.findMany({
-        where: (eq(projects.id, parseInt(projectId))),
+        where: eq(projects.id, parseInt(projectId)),
         with: {
-            feedbacks: true
-        }
-
+            feedbacks: true,
+        },
     });
 
     const project = userProjects[0];
 
-    return (
+    if (!project) {
+        return <div>Project not found</div>;
+    }
 
+    return (
         <div>
             <div>
                 <Link href="/dashboard" className="flex items-center text-primary mb-5 w-fit">
@@ -42,13 +45,15 @@ const page = async ({ params }: {
                     <h2 className="text-primary-background text-xl mb-2">{project.description}</h2>
                 </div>
                 <div className="flex flex-col">
-                    {project.url ?
-                        <Link href={project.url.startsWith('http') ? project.url : `http://${project.url}`} className="underline text-primary flex items-center">
+                    {project.url ? (
+                        <Link
+                            href={project.url.startsWith("http") ? project.url : `http://${project.url}`}
+                            className="underline text-primary flex items-center"
+                        >
                             <Globe className="h-5 w-5 mr-1" />
                             <span className="text-lg">Visit site</span>
                         </Link>
-                        : null
-                    }
+                    ) : null}
                     <Link href={`/projects/${projectId}/instructions`} className="underline text-primary flex items-center mt-2">
                         <Code className="h-5 w-5 mr-1" />
                         <span className="text-lg">Embed Code</span>
